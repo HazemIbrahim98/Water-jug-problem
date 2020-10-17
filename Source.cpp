17dabc1bbba82187dfdee4849881b00fd4b16477
Hazem Mohamed <hazem.moh98@gmail.com>
2020-10-18 01:35:38 +0200

    Updated the program to be more efficient :)

--- Source.cpp
+++ Source.cpp
@@ -1,115 +1,148 @@
 #include<stdio.h>
 
-struct node{
-	
+int finishedPoints[50][2];
+int index = 0;
+
+int jugsMaxCap[2];
+int wantToFind[2];
+
+struct node {
 	int state[2];
 
 	node* children[6];
 	bool deadend;
 
-	node(){
+	node() {
 		state[0] = 0;
 		state[1] = 0;
+
 		deadend = false;
 	}
 };
 
+node* duplicateNode(node* currentNode, int i) {
+	node* temp = new node;
 
-node* duplicateNode(node* currentNode, int i){
-	node *temp = new node;
-
-
-
-	temp -> state[0] = currentNode->state[0];
-	temp -> state[1] = currentNode->state[1];
+	temp->state[0] = currentNode->state[0];
+	temp->state[1] = currentNode->state[1];
+	temp->deadend = false;
 
 	return temp;
 }
 
-
-
-void one_to_two(node* currentNode){
-	
-	while (currentNode -> state[1] < 3)
-	{
-		if(currentNode -> state[0] != 0){
-			currentNode -> state[0] --;
-			currentNode -> state[1]++;
+void fill(node* currentNode, int index) {
+	currentNode->state[index] = jugsMaxCap[index];
+}
+void swap(node* currentNode, int index) {
+	if (index == 0) {
+		while (currentNode->state[1] < 3)
+		{
+			if (currentNode->state[0] != 0) {
+				currentNode->state[0] --;
+				currentNode->state[1]++;
+			}
+			else
+				break;
 		}
-		else
-			break;	
+	}
+	else {
+		while (currentNode->state[0] < 4)
+		{
+			if (currentNode->state[1] != 0) {
+				currentNode->state[1] --;
+				currentNode->state[0]++;
+			}
+			else
+				break;
+		}
+
 	}
 }
+void empty(node* currentNode, int index) {
+		currentNode->state[index] = 0;
+}
 
-void two_to_one(node* currentNode){
-	
-	while (currentNode -> state[0] < 4)
-	{
-		if(currentNode -> state[1] != 0){
-			currentNode -> state[1] --;
-			currentNode -> state[0]++;
-		}
-		else
-			break;
+bool repeated(node* currentNode) {
+	for (int i = 0; i < index; i++) {
+		if (finishedPoints[i][0] == currentNode->state[0] && finishedPoints[i][1] == currentNode->state[1])
+			return true;
 	}
+	return false;
 }
 
-void fill_One(node* currentNode){
-	currentNode -> state[0] = 4;
-}
-
-void fill_Two(node* currentNode){
-	currentNode -> state[1] = 3;
-}
-
-void empty_One(node* currentNode){
-	currentNode -> state[0] = 0;
-}
-
-void empty_Tow(node* currentNode){
-	currentNode -> state[1] = 0;
-}
-
-
-bool update(node* currentNode){
-
-	if(currentNode -> state[0] == 0)
-		if(currentNode -> state[1] == 1){
-			printf("yayyyy it worked");
+bool winCondition(node* currentNode) {
+	//	for each state element check if it is equal to the end
+	for (int i = 0; i < 2; i++)
+	{
+		if (currentNode->state[i] != wantToFind[i])
 			return false;
-		}
-	
-		for(int i = 0; i < 6; i++)
-			 currentNode -> children[i] = duplicateNode(currentNode, i);
+	}
 
-	fill_One(currentNode -> children[0]);
-	fill_Two(currentNode -> children[1]);
-	one_to_two(currentNode -> children[2]);
-	two_to_one(currentNode -> children[3]);
-	empty_One(currentNode -> children[4]);
-	empty_Tow(currentNode -> children[5]);
-
-	//printf((currentNode.children[3]).state[0]);
-	//printf((currentNode.children[3]).state[1]);
-
+	printf("YAYYY I found it :p\n");
+	return true;
 }
 
+bool update(node* currentNode) {
+	if (winCondition(currentNode))
+		return true;
 
+	if (currentNode->deadend == true)
+		return false;
 
-void main(){
-	
+	if (repeated(currentNode)) {
+		currentNode->deadend = true;
+		return false;
+	}
+	else {
+		for (int i = 0; i < 6; i++)
+			currentNode->children[i] = duplicateNode(currentNode, i);
+
+		fill(currentNode->children[0], 0);
+		fill(currentNode->children[1], 1);
+		swap(currentNode->children[2], 0);
+		swap(currentNode->children[3], 1);
+		empty(currentNode->children[4], 0);
+		empty(currentNode->children[5], 1);
+	}
+
+	finishedPoints[index][0] = currentNode->state[0];
+	finishedPoints[index][1] = currentNode->state[1];
+	index++;
+
+	return false;
+}
+
+bool navigate(node* currentNode) {
+
+	if (update(currentNode) == true)
+	{
+		return true;
+	}
+
+	if (currentNode->deadend == false)
+		for (int i = 0; i < 6; i++)
+		{
+			if (navigate(currentNode->children[i]) == true)
+				return true;
+		}
+
+	return false;
+}
+
+int main() {
+
 	node main;
-		
-	int jugsMaxCap[2];
+
 	jugsMaxCap[0] = 4;
 	jugsMaxCap[1] = 3;
 
-	update(&main);
+	wantToFind[0] = 0;
+	wantToFind[1] = 1;
 
-	update(&main);
 
-	update(&main);
+	printf("Starting\n");
+	navigate(&main);
+	printf("Done :)\n");
 
-	update(&main);
-
+	return 0;
 }
